f681ea3cd743c919d4cb4997b5edcff205eda5c6
GabrienConte <gabrielconte0@gmail.com>
2024-06-23 23:02:02 -0300

    adiciona plataforma  no retorno e ajsuta url da imagem

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from Sites import Sites
 
 def get_product_info(url):
@@ -9,16 +10,22 @@ def get_product_info(url):
     if Sites.BooksToScrape.value in url:
         title = soup.find('div', {'class': 'product_main'}).find('h1').text.strip()
         price = soup.find('p', {'class': 'price_color'}).text.strip()
-        image_url = soup.find('div', {'class': 'item active'}).find('img')['src']
-    if Sites.LojaVirtual.value in url:
+        image_relative_url = soup.find('div', {'class': 'item active'}).find('img')['src']
+        image_url = urljoin(url, image_relative_url)
+        plataforma = Sites.BooksToScrape.value
+    elif Sites.LojaVirtual.value in url:
         title = soup.find('h1', {'class': 'ui-pdp-title'}).text.strip()
         interiro = soup.find('span', {'class':'andes-money-amount__fraction'}).text.strip()
         fracao = soup.find('span', {'class':'andes-money-amount__cents andes-money-amount__cents--superscript-36'}).text.strip()
         price = f"{interiro}.{fracao}"
         image_url = soup.find('figure', {'class': 'ui-pdp-gallery__figure'}).find('img')['src']
+        plataforma = Sites.LojaVirtual.value
+    else:
+        plataforma = "Outra"
 
     return {
-        'title': title,
-        'price': price,
-        'image_url': image_url
+        'descricao': title,
+        'valor': price,
+        'imagemPath': image_url,
+        'plataforma': plataforma
     }
